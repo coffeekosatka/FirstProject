ecbefbce7140e154b35b37666ce8e4098aa72d5c
Nogasireny <viikkuullyy@gmail.com>
2019-09-23 00:45:13 +0300

    ДЗ2

---
+++ app/src/main/java/com/example/myapplication/DetailsActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+
+        val fbutton: Button = findViewById(R.id.button)
+        fbutton.setOnClickListener {
+            openWebPage("https://www.youtube.com/watch?v=6ZfuNTqbHE8")
+        }
+    }
+
+    fun openWebPage(url: String) {
+        val webpage: Uri = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+}
